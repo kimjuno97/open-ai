fa216f23620f5bc777203c447d6dd12fd3056614
kimjuno97 <jh241093@gmail.com>
2023-04-09 12:29:49 +0900

    fix: app/page import compile error 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import styled from 'styled-components';
 
-import IFrameReject from '@/components/IFrameReject';
-import KaKaoAdFit from '@/components/KaKaoAdFit';
+import { IFrameReject, KaKaoAdFit } from '@/components';
 
 import chat from '../../public/chat.svg';
 import image from '../../public/image.svg';
